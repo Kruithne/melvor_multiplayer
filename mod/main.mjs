81138284dffc746c4e3dfae9586fcf293fbf9e40
Kruithne <kruithne@gmail.com>
2024-09-06 11:18:08 +0100

    patch localization

--- mod/main.mjs
+++ mod/main.mjs
@@ -11,7 +11,36 @@ function notify(lang_id, theme = 'danger', icon = 'assets/archaeology.svg') {
 	notifyPlayer({ media: ctx.getResourceUrl(icon) }, getLangString(lang_id), theme);
 }
 
+/** Patches the global fetchLanguageJSON() fn so we can load and inject our own
+ * translations. This is a hackfix because I couldn't find a way for mods to load
+ * their own translations via data. */
+async function patch_localization(ctx) {
+	const lang_supported = ['en'];
+
+	const fetch_mod_localization = async (lang) => {
+		const fetch_lang = lang_supported.includes(lang) ? lang : 'en';
+
+		try {
+			const patch_lang = await ctx.loadData('data/lang/' + fetch_lang + '.json');
+			for (const [key, value] of Object.entries(patch_lang))
+				loadedLangJson[key] = value;
+		} catch (e) {
+			console.error('Failed to patch localization for %s (%s)', fetch_lang, e);
+		}
+	};
+
+	const orig_fetchLanguageJSON = globalThis.fetchLanguageJSON;
+	globalThis.fetchLanguageJSON = async (lang) => {
+		await orig_fetchLanguageJSON(lang);
+		await fetch_mod_localization(lang);
+	}
+
+	if (loadedLangJson !== undefined)
+		await fetch_mod_localization(setLang);
+}
+
 export async function setup(ctx) {
+	await patch_localization(ctx);
 	await ctx.loadTemplates('ui/templates.html');
 	
 	//ui.create({ $template: '#template-kru-archaeology-container', state	}, document.body);
