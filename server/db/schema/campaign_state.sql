09b104c87e49ee915a46a23307916b33f18970ea
Kruithne <kruithne@gmail.com>
2024-09-24 16:29:12 +0100

    fix type/sign order in table creation for campaign_state

--- server/db/schema/campaign_state.sql
+++ server/db/schema/campaign_state.sql
@@ -3,8 +3,8 @@ CREATE TABLE `campaign_state` (
 	`id` SERIAL,
 	`campaign_id` VARCHAR(20) NOT NULL,
 	`item_id` VARCHAR(255) NOT NULL,
-	`item_amount` UNSIGNED INT NOT NULL,
-	`item_current` UNSIGNED INT NOT NULL DEFAULT 0,
-	`complete` UNSIGNED TINYINT NOT NULL DEFAULT 0,
-	`campaign_next` UNSIGNED BIGINT NOT NULL DEFAULT 0
+	`item_amount` INT UNSIGNED NOT NULL,
+	`item_current` INT UNSIGNED NOT NULL DEFAULT 0,
+	`complete` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+	`campaign_next` BIGINT UNSIGNED NOT NULL DEFAULT 0
 );
