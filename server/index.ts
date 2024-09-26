dd06438e4b466b8c5609ca9e7b43828e2230d93b
Kruithne <kruithne@gmail.com>
2024-09-26 15:02:15 +0100

    fix order of operations in sql query

--- server/index.ts
+++ server/index.ts
@@ -585,7 +585,7 @@ function session_post_route(route: string, handler: SessionRequestHandler) {
 // #region ROUTES CAMPAIGN
 session_get_route('/api/campaign/info', async (req, url, client_id) => {
 	const history = await db_get_all('SELECT a.`item_amount`, a.`taken`, b.`id`, b.`campaign_id`, b.`item_id` FROM `campaign_contributions` AS a JOIN `campaign_state` AS b ON a.`campaign_id` = b.`id` WHERE a.`client_id` = ? AND b.`complete` = 1 ORDER BY a.`campaign_id` DESC LIMIT 15', [client_id]);
-	const rankings = await db_get_all('SELECT b.`campaign_id`, COUNT(*) AS `completed` FROM `campaign_contributions` AS a JOIN `campaign_state` AS b ON a.`campaign_id` = b.`id` GROUP BY b.`campaign_id` WHERE a.`client_id` = ?', [client_id]);
+	const rankings = await db_get_all('SELECT b.`campaign_id`, COUNT(*) AS `completed` FROM `campaign_contributions` AS a JOIN `campaign_state` AS b ON a.`campaign_id` = b.`id` WHERE a.`client_id` = ? GROUP BY b.`campaign_id`', [client_id]);
 
 	if (campaign_active_id > 0) {
 		const contribution = await db_get_single(
