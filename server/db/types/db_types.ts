5b3efe1acbcf3c8c03273f1683e31d97b4a0f591
Kruithne <kruithne@gmail.com>
2024-09-28 10:39:47 +0100

    remove null potential from database types

--- server/db/types/db_types.ts
+++ server/db/types/db_types.ts
@@ -6,17 +6,17 @@ export type campaign_state = {
 	item_current: number;
 	campaign_next: number;
 	complete: number;
-} | null;
+};
 
 export type charity_items = {
 	item_id: string;
 	qty: number;
-} | null;
+};
 
 export type client_sessions = {
 	session_token: string,
 	client_id: number
-} | null;
+};
 
 export type clients = {
 	id: number,
@@ -27,39 +27,39 @@ export type clients = {
 	icon_id: string,
 	last_charity: number,
 	last_bonus_charity: number
-} | null;
+};
 
 export type friend_requests = {
 	request_id: number,
 	client_id: number,                   
 	friend_id: number
-} | null;
+};
 
 export type friends = {
 	client_id_a: number;
 	client_id_b: number;
-} | null;
+};
 
 export type gift_items = {
 	id: number;
 	gift_id: number;
 	item_id: string;
 	qty: number;
-} | null;
+};
 
 export type gifts = {
 	gift_id: number;
 	client_id: number;
 	sender_id: number;
 	flags: number;
-} | null;
+};
 
 export type resolved_trade_offers = {
 	trade_id: number;
 	client_id: number;
 	sender_id: number;
 	declined: number;
-} | null;
+};
 
 export type trade_items = {
 	id: number;
@@ -67,7 +67,7 @@ export type trade_items = {
 	item_id: string;
 	qty: number;
 	counter: number;
-} | null;
+};
 
 export type trade_offers = {
 	trade_id: number;
@@ -75,14 +75,14 @@ export type trade_offers = {
 	recipient_id: number;
 	attending_id: number;
 	state: number;
-} | null;
+};
 
 export type campaign_contributions = {
 	campaign_id: number;
 	client_id: number;
 	item_amount: number;
 	taken: number;
-} | null;
+};
 
 export type market_items = {
 	id: number;
@@ -90,4 +90,4 @@ export type market_items = {
 	qty: number;
 	sold: number;
 	price: number;
-} | null;
+};
