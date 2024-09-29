3703867dfa2c119e9ddf55d64fad5eee650b73db
Kruithne <kruithne@gmail.com>
2024-09-29 14:29:01 +0100

    designate campaign items

--- server/campaign_data.ts
+++ server/campaign_data.ts
@@ -7,39 +7,138 @@ export const AVAILABLE_CAMPAIGNS = [
 	{
 		id: 'campaign_desert',
 		items: [
-			'melvorF:Small_Urn',
-			'melvorF:Medium_Urn',
+			'melvorD:Topaz',
 			'melvorD:Sapphire',
+			'melvorD:Ruby',
+			'melvorD:Emerald',
+			'melvorD:Diamond',
+			'melvorD:Silver_Bar',
+			'melvorD:Gold_Bar',
+			'melvorD:Small_Urn',
+			'melvorD:Medium_Urn'
 		]
 	},
 	{
 		id: 'campaign_snow',
 		items: [
-			'melvorD:Sapphire'
+			'melvorD:Rune_Essence',
+			'melvorD:Air_Rune',
+			'melvorD:Mind_Rune',
+			'melvorD:Water_Rune',
+			'melvorD:Earth_Rune',
+			'melvorD:Fire_Rune',
+			'melvorD:Body_Rune',
+			'melvorD:Chaos_Rune',
+			'melvorD:Death_Rune',
+			'melvorD:Blood_Rune',
+			'melvorD:Ancient_Rune',
+			'melvorD:Raw_Shrimp',
+			'melvorD:Raw_Sardine',
+			'melvorD:Raw_Herring',
+			'melvorD:Raw_Trout',
+			'melvorD:Raw_Salmon',
+			'melvorD:Raw_Lobster',
+			'melvorD:Raw_Swordfish',
+			'melvorD:Raw_Crab',
+			'melvorD:Raw_Shark',
+			'melvorD:Raw_Cave_Fish',
+			'melvorD:Raw_Manta_Ray',
+			'melvorD:Raw_Whale',
+			'melvorD:Shrimp',
+			'melvorD:Sardine',
+			'melvorD:Herring',
+			'melvorD:Trout',
+			'melvorD:Salmon',
+			'melvorD:Lobster',
+			'melvorD:Swordfish',
+			'melvorD:Crab',
+			'melvorD:Shark',
+			'melvorD:Cave_Fish',
+			'melvorD:Manta_Ray',
+			'melvorD:Whale'
 		]
 	},
 	{
 		id: 'campaign_forest',
 		items: [
-			'melvorD:Sapphire'
+			'melvorF:Poraxx_Herb',
+			'melvorF:Pigtayle_Herb',
+			'melvorF:Barrentoe_Herb',
+			'melvorF:Poraxx_Seed',
+			'melvorF:Pigtayle_Seed',
+			'melvorF:Barrentoe_Seed',
+			'melvorD:Garum_Herb',
+			'melvorD:Sourweed_Herb',
+			'melvorD:Mantalyme_Herb',
+			'melvorD:Lemontyle_Herb',
+			'melvorD:Oxilyme_Herb',
+			'melvorD:Garum_Seed',
+			'melvorD:Sourweed_Seed',
+			'melvorD:Mantalyme_Seed',
+			'melvorD:Lemontyle_Seed',
+			'melvorD:Oxilyme_Seed',
+			'melvorD:Potato_Seed',
+			'melvorD:Onion_Seed',
+			'melvorD:Cabbage_Seed',
+			'melvorD:Tomato_Seed',
+			'melvorD:Sweetcorn_Seed',
+			'melvorD:Strawberry_Seed',
+			'melvorD:Watermelon_Seed',
+			'melvorD:Snape_Grass_Seed',
+			'melvorD:Bird_Nest'
 		]
 	},
 	{
 		id: 'campaign_forsaken',
 		items: [
-			'melvorD:Sapphire'
+			'melvorD:Bones',
+			'melvorD:Dragon_Bones',
+			'melvorD:Magic_Bones',
+			'melvorD:Big_Bones',
+			'melvorD:Bronze_Arrows',
+			'melvorD:Iron_Arrows',
+			'melvorD:Steel_Arrows',
+			'melvorD:Mithril_Arrows',
+			'melvorD:Adamant_Arrows',
+			'melvorD:Rune_Arrows',
+			'melvorD:Dragon_Arrows',
+			'melvorD:Bronze_Arrowtips',
+			'melvorD:Iron_Arrowtips',
+			'melvorD:Steel_Arrowtips',
+			'melvorD:Mithril_Arrowtips',
+			'melvorD:Adamant_Arrowtips',
+			'melvorD:Rune_Arrowtips',
+			'melvorD:Dragon_Arrowtips'
 		]
 	},
 	{
 		id: 'campaign_jungle',
 		items: [
-			'melvorD:Sapphire'
+			'melvorD:Normal_Logs',
+			'melvorD:Oak_Logs',
+			'melvorD:Willow_Logs',
+			'melvorD:Teak_Logs',
+			'melvorD:Maple_Logs',
+			'melvorD:Mahogany_Logs',
+			'melvorD:Yew_Logs',
+			'melvorD:Magic_Logs',
+			'melvorD:Redwood_Logs'
 		]
 	},
 	{
 		id: 'campaign_volcanic',
 		items: [
-			'melvorD:Sapphire'
+			'melvorD:Copper_Ore',
+			'melvorD:Tin_Ore',
+			'melvorD:Iron_Ore',
+			'melvorD:Coal_Ore',
+			'melvorD:Silver_Ore',
+			'melvorD:Gold_Ore',
+			'melvorD:Mithril_Ore',
+			'melvorD:Adamantite_Ore',
+			'melvorD:Runite_Ore',
+			'melvorD:Dragonite_Ore',
+			'melvorF:Ash'
 		]
 	}
 ];
